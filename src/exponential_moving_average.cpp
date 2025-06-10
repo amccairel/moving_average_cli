26118e12863aa5363d897775649c4ba165213e8b
amccairel <mccairelaw@gmail.com>
2025-06-09 22:44:14 -0600

    -updated README.md (#2)
    -added weighted_moving_average.cpp and weighted_moving_average.h
    -cleaned up a bit

--- src/exponential_moving_average.cpp
+++ src/exponential_moving_average.cpp
@@ -1,14 +1,14 @@
 //
 // Created by Alexander McCairel on 5/25/25.
 //
-#include "../include/exponential_moving_average.h"
+#include "exponential_moving_average.h"
 
 std::vector<double> ExponentialMovingAverage::calculate(const std::vector<double>& prices, const int& window) const {
     std::vector<double> averages;
     const double alpha = 2.0 / (window + 1);
 
     double last_ema = prices[0];
-    for (double price : prices) {
+    for (const double price : prices) {
         double new_ema = alpha * (price - last_ema) + last_ema;
         averages.push_back(new_ema);
         last_ema = new_ema;
